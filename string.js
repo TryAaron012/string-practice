b20dda198e00e5d2ad50ddc7946998995e001631
Aaron Raymund Paguirigan <paguiriganaaronraymund@gmail.com>
2024-09-15 13:39:00 +0800

    initial commit

--- string.js
+++ string.js
@@ -1,11 +1,13 @@
-let favActorFirstName = "Tom";
-let favActorLastName = "Hanks"; 
+let favActorFirstName = "Ryan";
+let favActorLastName = "Gosling"; 
+let favoritePetName = "Roady"; 
 
 let fullName = favActorFirstName + " " + favActorLastName;
 let uppercase = fullName.toLowerCase();
+let lowercase = favoritePetName.toUpperCase();
 
 let message = "My favorite actor is " + fullName.toUpperCase() + ".";
-message += " His best show is Silicon Valley.";
+message += " His best show was The Note book.";
 
 
 console.log("Favorite Actor First Name:", favActorFirstName);
@@ -13,3 +15,4 @@ console.log("Favorite Actor Last Name:", favActorLastName);
 console.log("Full Name:", fullName);
 console.log("Lowercase Version of Actor's Name:", uppercase);
 console.log("Message:", message);
+console.log('Fav Pet Name:', lowercase);
